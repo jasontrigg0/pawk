204da2a11426558f6f7fb37c9e4968580b458a0e
Jason Trigg <jasontrigg0@gmail.com>
2020-04-12 14:37:13 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+    name = 'python-awk',
+    packages = ['pawk'],
+    version = "0.0.5",
+    description = 'Placeholder description',
+    author = "Jason Trigg",
+    author_email = "jasontrigg0@gmail.com",
+    url = "https://github.com/jasontrigg0/pawk",
+    download_url = 'https://github.com/jasontrigg0/pawk/tarball/0.0.5',
+    scripts=["pawk/pawk"],
+    install_requires=["jtutils",
+                      "argparse",
+                      "six"
+    ],
+    keywords = [],
+    classifiers = [],
+)
